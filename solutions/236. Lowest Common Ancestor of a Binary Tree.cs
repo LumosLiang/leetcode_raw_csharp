b767527817fb6c4f8636cf8795dc8e4b9fab57ef
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-06-08 15:32:43 +0800

    124 ms

---
+++ solutions/236. Lowest Common Ancestor of a Binary Tree.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root == p || root == q || root == null) return root;
+        
+        TreeNode left = LowestCommonAncestor(root.left, p, q);
+        TreeNode right = LowestCommonAncestor(root.right, p, q);
+        
+        if(left != null && right != null) return root;
+​
+        if(left != null) return left;
+        else return right;
+​
+    }
+}
