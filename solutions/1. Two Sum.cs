c14d9935c250762b3f568063f77855fdb3744fea
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-06-08 15:29:42 +0800

    232 ms

---
+++ solutions/1. Two Sum.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> hash = new Dictionary<int, int>();
+        
+        for(int i = 0; i< nums.Count(); i++)
+        {
+            if(hash.ContainsKey(nums[i]))
+            {
+                return new int[]{i,hash[nums[i]]};
+            }
+            else
+            {
+                hash.Add(target - nums[i], i);
+            }
+        }
+        return new int[2];
+​
+    }
+}
