664c7c7cd149ff4981c7866240a6e85964d660a8
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-07-10 20:57:39 +0800

    312 ms

--- solutions/46. Permutations.cs
+++ solutions/46. Permutations.cs
@@ -1,29 +1,31 @@
 public class Solution {
+    
+    IList<IList<int>> res = new List<IList<int>>();
+    
     public IList<IList<int>> Permute(int[] nums) {
-        var res = new List<IList<int>>();
         
-        void backtrack(int[] choices, List<int> path)
-        {
-            if(choices.Length == 0)
-            {
-                res.Add(new List<int>(path));
-                return;
-            }
-            for(int i = 0; i < choices.Length; i++)
-            {
-                path.Add(choices[i]);
-                ArraySegment<int> c1 = new ArraySegment<int>( choices, 0, i);
-                ArraySegment<int> c2 = new ArraySegment<int>( choices, i + 1, choices.Length - i - 1);
-                choices = c1.ToArray().Concat(c2.ToArray()).ToArray();
-                backtrack(choices, path);
-            }
-            
-        }
-        var p= new List<int>();
-        backtrack(nums, p);
+        helper(new List<int>(nums), new List<int>());
         return res;
+    }
+    
+    public void helper(List<int> choices, List<int> path)
+    {
+        if(choices.Count == 0)
+        {
+            res.Add(path);
+            return
+        }
+        
+        for(int i = 0; i < choices.Count; i++)
+        {
+            var tpath = new List<int>(path);
+            tpath.Add(choices[i]);
+            var tchoices = new List<int>(choices);
+            tchoices.RemoveAt(i);
+            helper(tchoices, tpath);
+        }
+        
     }
 }
 ​
 ​
-        
