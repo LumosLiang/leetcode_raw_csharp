37c9cc7508c03a20501df960d2b1dd07fae885b7
lumos! <43442549+LumosLiang@users.noreply.github.com>
2021-11-06 14:38:54 +0800

    172 ms

---
+++ solutions/35. Search Insert Position.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        
+        int l = 0, r = nums.Length;
+        
+        while(l < r)
+        {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] < target) l = mid + 1;
+            else r = mid;
+        }
+        
+        return l;
+    }
+}
