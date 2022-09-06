6ffbdffaeb91f020a991bd08be3ef0611c73fcd8
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-06 17:22:23 +0800

    265 ms

--- solutions/15. 3Sum.cs
+++ solutions/15. 3Sum.cs
@@ -1,50 +1,37 @@
 public class Solution {
-    public IList<IList<int>> ThreeSum(int[] nums) {
-        
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+​
         Array.Sort(nums);
-        var res = new List<IList<int>>();
+        IList<IList<int>> res = new List<IList<int>>();
         var len = nums.Length;
         if (len < 3) return res;
-        
-        for(int i = 0; i < len - 2; i++)
+​
+        for (int i = 0; i < len - 2; i++)
         {
-            if(i > 0 && nums[i] == nums[i - 1])
-            {
-                continue;
-            }
-            
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+​
             int l = i + 1;
             int r = len - 1;
-            int target = 0 - nums[i];
-            
-            while(l < r)
+​
+            while (l < r)
             {
-                var sum = nums[l] + nums[r];
-                if(sum == target)
+                var sum = nums[l] + nums[r] + nums[i];
+                if (sum == 0)
                 {
-                    res.Add(new List<int>(){nums[i], nums[l], nums[r]});
-                    while(l < r && nums[l] == nums[l + 1])
-                    {
-                        l++;
-                    }
-                    while(l < r && nums[r] == nums[r - 1])
-                    {
-                        r--;
-                    }
+                    res.Add(new List<int>() { nums[i], nums[l], nums[r] });
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+​
                     l += 1;
                     r -= 1;
                 }
-                else if(sum < target)
-                {
-                    l++;
-                }
-                else
-                {
-                    r--;
-                }
+                else if (sum < 0) l++;
+                else r--;
             }
         }
         return res;
-    }
+    }   
 }
+​
 ​
