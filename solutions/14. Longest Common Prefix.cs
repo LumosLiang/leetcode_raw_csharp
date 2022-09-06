8bd425bf3e8f6db6a15240c7717b6e71ea16d245
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-06 17:15:31 +0800

    101 ms

--- solutions/14. Longest Common Prefix.cs
+++ solutions/14. Longest Common Prefix.cs
@@ -1,31 +1,32 @@
 public class Solution {
-    public string LongestCommonPrefix(string[] strs)
-    {
-        if (strs.Length == 1) return strs[0];
-​
-        var res = "";
-        int p = 0;
-        while (IsEqual(strs, p))
+    public string LongestCommonPrefix(string[] strs) {
+     
+        var res = strs[0];
+        
+        for(int i = 1; i < strs.Length; i++)
         {
-            res += strs[0][p];
-            p += 1;
+            res =  GetCommon(res, strs[i]);
+        }
+        
+        return res;
+    }
+    
+    private static string GetCommon(string string1, string string2)
+    {
+        int p1 = 0, p2 = 0;
+        string res = "";
+        while(p1 < string1.Length && p2 < string2.Length)
+        {
+            if(string1[p1] == string2[p2])
+            {
+                res += string1[p1];
+                p1++;
+                p2++;
+            }
+            else break;
         }
         return res;
     }
-​
-    private bool IsEqual(string[] strs, int p)
-    {
-        var res = true;
-​
-        for (int i = 0; i < strs.Length - 1; i++)
-        {
-            if (strs[i].Substring(p, strs[i].Length - p) != "" && strs[i + 1].Substring(p, strs[i + 1].Length - p) != "" && strs[i][p] == strs[i + 1][p])
-            {
-                continue;
-            }
-            else return false;
-        }
-​
-        return res;
-    }
 }
+​
+​
