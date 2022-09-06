592979bb78467e135df5f3534a8195433d2dc9c1
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-06 15:29:45 +0800

    93 ms

--- solutions/3. Longest Substring Without Repeating Characters.cs
+++ solutions/3. Longest Substring Without Repeating Characters.cs
@@ -1,25 +1,21 @@
 public class Solution {
-    public int LengthOfLongestSubstring(string s)
-    {
-​
+    public int LengthOfLongestSubstring(string s) {
+        
         int left = 0, res = 0;
         Dictionary<int, int> window = new Dictionary<int, int>();
 ​
         for (int right = 0; right < s.Length; right++) 
         {
-            if (window.ContainsKey(s[right]) && left <= window[s[right]])
+            if (window.ContainsKey(s[right]))
             {
-                res = Math.Max(res, right - left);
-                left = window[s[right]] + 1;
-            }
-            else
-            { 
-                res = Math.Max(res, right - left + 1);
+                left = Math.Max(left, window[s[right]] + 1);
             }
 ​
             window[s[right]] = right;
+            res = Math.Max(res, right - left + 1);
         }
-​
         return res;
+        
     }
 }
+​
