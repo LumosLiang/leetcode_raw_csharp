56bfe40debbbb99935a60f1cf6b7ee77f040e122
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-06 15:22:23 +0800

    120 ms

--- solutions/2. Add Two Numbers.cs
+++ solutions/2. Add Two Numbers.cs
@@ -11,28 +11,29 @@
  */
 public class Solution {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
-    {
+    {
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
-        int sum = 0;
-        while (l1 != null || l2 != null || sum != 0)
+        int carry = 0;
+​
+        while (l1 != null || l2 != null || carry != 0)
         {
             if (l1 != null)
             {
-                sum += l1.val;
+                carry += l1.val;
                 l1 = l1.next;
             }
             if (l2 != null)
             {
-                sum += l2.val;
+                carry += l2.val;
                 l2 = l2.next;
             }
 ​
-            curr.next = new ListNode(sum % 10);
-            sum /= 10;
+            curr.next = new ListNode(carry % 10);
             curr = curr.next;
+            carry /= 10;
         }
         return dummy.next;
 ​
-    }
+    }
 }
