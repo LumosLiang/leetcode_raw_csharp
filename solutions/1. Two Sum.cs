1b60862e189d55c5f448d475b4c84409f7abe511
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-06 15:16:10 +0800

    282 ms

--- solutions/1. Two Sum.cs
+++ solutions/1. Two Sum.cs
@@ -1,19 +1,42 @@
 public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> hash = new Dictionary<int, int>();
+    public int[] TwoSum(int[] nums, int target) 
+    {
         
-        for(int i = 0; i< nums.Count(); i++)
+        Dictionary<int, int> hash = new Dictionary<int, int>();
+​
+        for (int i = 0; i < nums.Length; i++)
         {
-            if(hash.ContainsKey(nums[i]))
+            if (hash.ContainsKey(nums[i]))
             {
-                return new int[]{i, hash[nums[i]]};
+                return new int[] { i, hash[nums[i]] };
             }
             else
             {
-                if(!hash.ContainsKey(target - nums[i])) hash.Add(target - nums[i], i);
+                if (!hash.ContainsKey(target - nums[i])) 
+                    hash.Add(target - nums[i], i);
+                else
+                    hash[target - nums[i]] = i;
             }
         }
         return new int[2];
+    }
+    
+    
+    public int[] TwoSum2(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        int l = 0, r = nums.Length - 1;
 ​
+        while (l < r)
+        {
+            int sum = nums[l] + nums[r];
+            if (sum == target) return new int[] { l, r };
+            else if (sum > target) r--;
+            else l++;
+        }
+​
+        return new int[2];
     }
 }
+​
+​
