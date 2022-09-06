648082c038ff253531041327a465e8e45986bc7a
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-06 16:04:39 +0800

    41 ms

--- solutions/7. Reverse Integer.cs
+++ solutions/7. Reverse Integer.cs
@@ -1,15 +1,26 @@
 public class Solution {
-    public int Reverse(int x) {
-        int res = 0;
+    public int Reverse(int x)
+    {
+        if(x == -Math.Pow(2, 31)) return 0;
         
-        while(x != 0)
+        int res = 0, baseNumber = 10, sign = 1;
+        if (x < 0) sign = -1;
+        x = Math.Abs(x);
+​
+        while (x != 0)
         {
-            int temp = res * 10 + x % 10;
-            if (temp / 10 != res) return 0;
-            res = temp;
-            x = x / 10;
+            var r = x % 10;
+            if (res > int.MaxValue / baseNumber || (res == int.MaxValue / baseNumber && r >= int.MaxValue - res * baseNumber))
+                return 0;
+​
+            res = res * baseNumber + r;
+​
+            //// check if overflow 1
+            //if (temp / 10 != res) return 0;
+​
+            x /= 10;
         }
-        return res;
-        
+        return res * sign;
     }
 }
+​
