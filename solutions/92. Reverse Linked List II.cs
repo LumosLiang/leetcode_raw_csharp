dd4331310d492e379bbc13ac539bc66b2b2742d3
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-09 16:14:40 +0800

    143 ms

---
+++ solutions/92. Reverse Linked List II.cs
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseBetween(ListNode head, int left, int right) {
+        
+        
+        if (head is null || head.next is null) return head;
+​
+        ListNode dummy = new ListNode
+        {
+            next = head
+        };
+​
+        ListNode curr = dummy;
+​
+        for (int cnt = 0; cnt < left - 1; cnt++)
+        {
+            curr = curr.next;
+        }
+​
+        var nxt = curr.next;
+​
+        // insert head
+        for (int cnt = 0; cnt < right - left; cnt++)
+        {
+            var temp = curr.next;
+            curr.next = nxt.next;
+            nxt.next = nxt.next.next;
+            curr.next.next = temp;
+        }
+​
+        return dummy.next;
+        
+    }
+}
+​
