26ed82dd985b6a7e48555a9058e6ed95b984a63c
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-09 11:54:11 +0800

    203 ms

---
+++ solutions/268. Missing Number.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int MissingNumber(int[] nums) {
+    
+        for (int i = 0; i < nums.Length; i++)
+        {
+            while (nums[i] != i && nums[i] <= nums.Length - 1 && nums[i] >= 0 && nums[i] != nums[nums[i]])
+            {
+                int idx = nums[i];
+                int temp = nums[idx];
+                nums[idx] = nums[i];
+                nums[i] = temp;
+            }
+        }
+​
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i != nums[i]) return i;
+        }
+​
+        return nums.Length;
+    }
+}
