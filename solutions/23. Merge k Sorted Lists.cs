3f66267f7fe16371980258983780db39b1bca088
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-09-09 15:02:40 +0800

    179 ms

--- solutions/23. Merge k Sorted Lists.cs
+++ solutions/23. Merge k Sorted Lists.cs
@@ -2,60 +2,41 @@
  * Definition for singly-linked list.
  * public class ListNode {
  *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
+ *     public listnode next;
+ *     public listnode(int val=0, listnode next=null) {
  *         this.val = val;
  *         this.next = next;
  *     }
  * }
  */
+​
 public class Solution {
-    public ListNode MergeKLists(ListNode[] lists)
+    public ListNode MergeKLists(ListNode[] lists) {
+        
+​
+        
+        PriorityQueue<ListNode, int> pq = new PriorityQueue<ListNode, int>();
+        
+        foreach(var head in lists)
         {
-            if (lists.Length == 0) return null;
-​
-            return Merge(lists, 0, lists.Length - 1);
+            if(head != null) pq.Enqueue(head, head.val);
         }
-​
-        private ListNode Merge(ListNode[] lists, int left, int right)
+        
+        var res = new ListNode();
+        var curr = res;
+        
+        while(pq.Count != 0)
         {
-​
-            if (left == right) return lists[left];
-​
-            int mid = left + (right - left) / 2;
-​
-            var l = Merge(lists, left, mid);
-            var r = Merge(lists, mid + 1, right);
-​
-            return Merge(l, r);
+            var node = pq.Dequeue();
+            if(node.next != null) pq.Enqueue(node.next, node.next.val);
+            
+            curr.next = node;
+            curr = curr.next;
+            // node.next = null;
         }
-​
-        private ListNode Merge(ListNode A, ListNode B)
-        {
-            if (A == null) return B;
-            if (B == null) return A;
-​
-            ListNode dummy = new ListNode(0);
-            ListNode currA = A, currB = B, curr = dummy;
-​
-            while (currA != null && currB != null)
-            {
-                if (currA.val <= currB.val)
-                {
-                    curr.next = new ListNode(currA.val);
-                    currA = currA.next;
-                }
-                else 
-                {
-                    curr.next = new ListNode(currB.val);
-                    currB = currB.next;
-                }
-                curr = curr.next;
-            }
-​
-            if (currA != null) curr.next = currA;
-            else curr.next = currB;
-​
-            return dummy.next;
-        }
+        
+        return res.next;
+        
+    }
 }
+​
