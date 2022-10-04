ed70fa2e46b524bd1c85387852cc2a7909241092
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-10-04 18:11:16 +0800

    249 ms

---
+++ solutions/27. Remove Element.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        
+        int slow = 0;
+        
+        for(int fast = 0; fast < nums.Length; fast++)
+        {
+            if(nums[fast] != val)
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        
+        return slow;
+        
+    }
+}
