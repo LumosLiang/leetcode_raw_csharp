e81b67308c35813dfa55266c03e714baf0d2e0f8
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-10-06 17:18:06 +0800

    369 ms

---
+++ solutions/204. Count Primes.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int CountPrimes(int n) {
+     
+        return Solution2(n);
+    }
+    
+    public int Solution1(int n) {
+     
+        var dp = new int[n];
+        
+        for(int i = 2; i < n; i++)
+        {
+            if(dp[i] == 0)
+            {
+                for(int j = 2 * i; j < n; j += i)
+                    dp[j] = 1;
+            }
+        }
+        
+        return n >= 2? n - dp.Sum() - 2:0;
+        
+    }
+    
+    public int Solution2(int n) {
+     
+        var dp = new int[n];
+        
+        for(int i = 2; i < Math.Sqrt(n); i++)
+        {
+            if(dp[i] == 0)
+            {
+                for(int j = i * i; j < n; j += i)
+                    dp[j] = 1;
+            }
+        }
+        
+        return n >= 2? n - dp.Sum() - 2:0;  
+    }
+}
