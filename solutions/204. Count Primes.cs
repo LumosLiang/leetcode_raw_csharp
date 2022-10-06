2926138794019022f6e0c4f3630d992798da9efc
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-10-06 17:22:57 +0800

    369 ms

--- solutions/204. Count Primes.cs
+++ solutions/204. Count Primes.cs
@@ -4,22 +4,27 @@ public class Solution {
         return Solution2(n);
     }
     
+    // dp
+    //
     public int Solution1(int n) {
      
         var dp = new int[n];
         
         for(int i = 2; i < n; i++)
-        {
+        {
             if(dp[i] == 0)
             {
                 for(int j = 2 * i; j < n; j += i)
                     dp[j] = 1;
             }
-        }
+        }
         
         return n >= 2? n - dp.Sum() - 2:0;
         
     }
+    
+    // sieve of eratosthenes,
+    // 
     
     public int Solution2(int n) {
      
