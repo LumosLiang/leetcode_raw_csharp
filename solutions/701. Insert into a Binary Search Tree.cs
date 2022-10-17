b0d2e5505ec67a840d100cb924bb4760b3e62c1c
lumos! <43442549+LumosLiang@users.noreply.github.com>
2022-10-17 16:13:39 +0800

    216 ms

--- solutions/701. Insert into a Binary Search Tree.cs
+++ solutions/701. Insert into a Binary Search Tree.cs
@@ -12,7 +12,37 @@
  * }
  */
 public class Solution {
-    public TreeNode InsertIntoBST(TreeNode root, int val)
+    public TreeNode InsertIntoBST(TreeNode root, int val) {
+     
+        return InsertIntoBST1(root, val);
+    }
+​
+    public TreeNode InsertIntoBST1(TreeNode root, int val)
+    {
+        // base
+        if (root is null) return new TreeNode(val);
+​
+        TreeNode pre = null, curr = root;
+        
+        while(curr != null)
+        {
+            pre = curr;
+            if(curr.val > val)
+                curr = curr.left;
+            else
+                curr = curr.right;
+        }
+        if(pre.val > val)
+            pre.left = new TreeNode(val);
+        else
+            pre.right = new TreeNode(val);
+        
+        return root;
+        
+    }
+    
+    
+    public TreeNode InsertIntoBST2(TreeNode root, int val)
     {
         // base
         if (root is null) return new TreeNode(val);
@@ -24,4 +54,6 @@ public class Solution {
       
         return root;
     }
+    
 }
+​
